f8bd4fddc19fce19950fb8f837bf32800330d244
PHS <83255249+gus5963@users.noreply.github.com>
2022-03-04 11:14:52 +0900

    user연결

--- MBTIgg/hobbyApp/views.py
+++ MBTIgg/hobbyApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import *
 from mbtiApp.models import *
+from userApp.models import *
 
 # Create your views here.
 
@@ -9,10 +10,13 @@ def index(request):
     print('✅ GET Hobby Index 🚀')
     mbti_id = request.GET['mbti']
     hobbys = Hobby.objects.all()
+    request.session.get('user_mbti')
+    request.session.get('user_name')
     context={
         'mbti_id' : mbti_id,
-        'hobbys' : hobbys
+        'hobbys' : hobbys,
     }
+
     return render(request, 'hobby/index.html',context)
 
 
